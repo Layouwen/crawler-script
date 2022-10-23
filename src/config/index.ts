b9c4e5db09302b2649f54457431d711393248304
Avan <layouwen@gmail.com>
2022-10-23 11:16:14 +0800

    feat: 爬取论述题

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,4 +1,4 @@
-export const token = 'MjLOlQeZfk66BfBtJxUIpSi9vnobkSpMUey_JdxfT8I';
+export const token = 'jN4LGYiz6hineRGDly1AGHF_P9hs5Zb8FNEBcrPCdog';
 export const subject_id = '4f7a5ed0-5156-4942-a8cc-ad5af08df5e5'; // 马原
 // export const subject_id = '8470c4ee-4c3d-45f5-baf0-db71233cdf59' // 中国近代史
 export const sheet_id = '4e933e04-81a4-410a-a643-b9057bc89147';
--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,15 @@ import * as path from 'path';
 import { subject_id } from './config';
 import request from './utils/request';
 
-const outputPath = path.resolve(__dirname, '../output/马克思主义/历史真题');
+const outputPath = path.resolve(__dirname, '../output/马克思主义/应用题');
 
 enum CategoryName {
   CHOICE1 = '单项选择题',
   CHOICE2 = '单选题',
-  SUBJECTIVE = '主观题'
+  MORE_CHOICE = '多选题',
+  SUBJECTIVE = '主观题',
+  SHORT_ANSWER = '简答题',
+  DISCUSS = '论述题'
 }
 
 const getExamPaperPageList = async () => {
@@ -95,7 +98,7 @@ const getExamPaperTopicPageList = async (paper_id: string) => {
   };
 };
 
-const getTemplate = (data: any, hasNo = false) => {
+const getTemplate = (data: any, hasNo = false, type = 0) => {
   const {
     topic_no,
     topic_title,
@@ -103,13 +106,23 @@ const getTemplate = (data: any, hasNo = false) => {
     itemList,
     analysis,
   } = data;
-  let title = `${topic_title}（${answer.join('').toString()}）\n`;
-  if (hasNo) {
-    title = `${topic_no}、` + title;
+  if (type === 0) {
+    let title = `${topic_title}（${answer.join('').toString()}）\n`;
+    if (hasNo) {
+      title = `${topic_no}、` + title;
+    }
+    const items = itemList.map((i: { item_no: string; content: string; }) => `${i.item_no} ${i.content}\n`).join('');
+    const bottom = `解析：${analysis}\n`;
+    return title + items + bottom;
+  } else if (type === 1) {
+    let title = `${topic_title}\n`;
+    if (hasNo) {
+      title = `${topic_no}、` + title;
+    }
+    const items = itemList.map((i: { item_no: string; content: string; }) => `${i.item_no} ${i.content}\n`).join('');
+    const bottom = `答案：${answer.join('').toString()}\n`;
+    return title + items + bottom;
   }
-  const items = itemList.map((i: { item_no: string; content: string; }) => `${i.item_no} ${i.content}\n`).join('');
-  const bottom = `解析：${analysis}\n`;
-  return title + items + bottom;
 };
 
 const outputList = async ({name = '', sheet_id = '', paper_id = ''}) => {
@@ -117,23 +130,25 @@ const outputList = async ({name = '', sheet_id = '', paper_id = ''}) => {
     fs.mkdirSync(outputPath);
   }
 
-  let data
+  let data;
   if (sheet_id) {
     data = await getErrorTopicAnalysisPageList(sheet_id);
   }
   if (paper_id) {
-    data = await getExamPaperTopicPageList(paper_id)
+    data = await getExamPaperTopicPageList(paper_id);
   }
 
   if (data?.list) {
-    generateChoice(name, data.list);
+    // generateChoice(name, data.list);
+    generateApplied(name, data.list);
+    // 简答题
   }
   return 'success';
 };
 
 const generateChoice = (title: string, list: AAA[]) => {
   const listData = list
-    .filter(l => l.category_name.includes(CategoryName.CHOICE1) || l.category_name.includes(CategoryName.CHOICE2))
+    .filter(l => l.category_name.includes(CategoryName.CHOICE1) || l.category_name.includes(CategoryName.CHOICE2) || l.category_name.includes(CategoryName.MORE_CHOICE))
     .map(({topic_no, topic_title, answer, itemList, analysis}) => ({
       topic_no,
       topic_title,
@@ -142,7 +157,27 @@ const generateChoice = (title: string, list: AAA[]) => {
       analysis,
     }));
   const titleStr = `${title}（选择题）\n\n`;
-  const str = titleStr + listData.map(l => getTemplate(l)).join('\n');
+  const str = titleStr + listData.map(l => getTemplate(l, false, 0)).join('\n');
+
+  fs.writeFile(path.resolve(outputPath, `${title}.txt`), str, (err) => {
+    if (err) {
+      console.log(err);
+    }
+  });
+};
+
+const generateApplied = (title: string, list: AAA[]) => {
+  const listData = list
+    .filter(l => l.category_name.includes(CategoryName.DISCUSS) || l.category_name.includes(CategoryName.SHORT_ANSWER))
+    .map(({topic_no, topic_title, answer, itemList, analysis}) => ({
+      topic_no,
+      topic_title,
+      answer,
+      itemList,
+      analysis,
+    }));
+  const titleStr = `${title}\n\n`;
+  const str = titleStr + listData.map(l => getTemplate(l, false, 1)).join('\n');
 
   fs.writeFile(path.resolve(outputPath, `${title}.txt`), str, (err) => {
     if (err) {
@@ -156,14 +191,14 @@ const generateChoice = (title: string, list: AAA[]) => {
   paperList.forEach(p => {
     void outputList({
       name: p.name,
-      sheet_id: p.sheet_id
+      sheet_id: p.sheet_id,
     });
   });
   const data = await getExamPaperPageList2();
   data.forEach(p => {
     void outputList({
       name: p.name,
-      paper_id: p.paper_id
+      paper_id: p.paper_id,
     });
   });
 })();
