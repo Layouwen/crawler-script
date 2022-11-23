5c836033da3fdeda1daad4712edfec034c2dd627
Avan <layouwen@gmail.com>
2022-11-23 16:24:46 +0800

    chore: 删除无用代码

--- src/config/path.ts
+++ src/config/path.ts
@@ -12,20 +12,6 @@ export const getOutputSubjectPath = (name: string, subjectName: string) => {
   return resolve(getOutputOrganizationPath(name), `${subjectName}`);
 };
 
-export const getOutputSubjectChoiceQuestionPath = (
-  name: string,
-  subjectName: string
-) => {
-  return resolve(getOutputSubjectPath(name, subjectName), "选择题");
-};
-
-export const getOutputSubjectAppliedProblemPath = (
-  name: string,
-  subjectName: string
-) => {
-  return resolve(getOutputSubjectPath(name, subjectName), "应用题");
-};
-
 // base dir
 export const outputDirPath = resolve(__dirname, "../../output");
 export const logsDirPath = resolve(__dirname, "../../logs");
