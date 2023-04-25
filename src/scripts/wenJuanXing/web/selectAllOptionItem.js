84517ec35dd1fd1aedf56033a2d02374c26ca2ac
Avan <layouwen@gmail.com>
2023-04-25 18:14:43 +0800

    feat: 添加问卷星单选题自动选择脚本

--- src/scripts/wenJuanXing/web/selectAllOptionItem.js
+++ src/scripts/wenJuanXing/web/selectAllOptionItem.js
@@ -2,7 +2,14 @@
  * auto select all option item
  */
 
-const $allWrapper = document.querySelectorAll(".ui-controlgroup");
-$allWrapper.forEach((i) => {
-  i.childNodes[0].click();
-});
+// 多选题
+const $allGroupWrapper = document.querySelectorAll('.ui-controlgroup')
+$allWrapper.forEach(i => {
+  i.childNodes[0].click()
+})
+
+// 单选题
+const $allRadioWrapper = document.querySelectorAll('.ulradiocheck')
+$allWrapper.forEach(i => {
+  i.childNodes[0].click()
+})
