65cd59698b39df53215a5c91f24bfe7050d4f6f5
Avan <layouwen@gmail.com>
2023-06-30 23:05:11 +0800

    fix: 问卷星自动答题

--- src/scripts/wenJuanXing/web/selectAllOptionItem.js
+++ src/scripts/wenJuanXing/web/selectAllOptionItem.js
@@ -3,13 +3,16 @@
  */
 
 // 多选题
-const $allGroupWrapper = document.querySelectorAll('.ui-controlgroup')
-$allWrapper.forEach(i => {
-  i.childNodes[0].click()
-})
+const $allGroupWrapper = document.querySelectorAll(".ui-controlgroup");
+$allGroupWrapper.forEach((i) => {
+  i.childNodes[0].click();
+});
 
 // 单选题
-const $allRadioWrapper = document.querySelectorAll('.ulradiocheck')
-$allWrapper.forEach(i => {
-  i.childNodes[0].click()
-})
+const $allRadioWrapper = document.querySelectorAll(".ulradiocheck");
+$allRadioWrapper.forEach((i) => {
+  i.childNodes[0].click();
+});
+
+document.querySelectorAll("textarea").forEach((i) => (i.value = "八楼啊吧"));
+document.querySelectorAll("input").forEach((i) => (i.value = "巴拉巴拉"));
