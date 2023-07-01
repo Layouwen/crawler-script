f20079e415829f354b9aa2cb8484f073a96caf8a
Avan <layouwen@gmail.com>
2023-07-01 08:09:30 +0800

    feat: add title

--- src/scripts/wenJuanXing/index.ts
+++ src/scripts/wenJuanXing/index.ts
@@ -1,36 +1,40 @@
 // 过滤不需要的题目
-const questionTypeArr = [
-  '【熟记】',
-  '【必会】',
-  '【理解】',
-  '【领会】',
-];
-const right = '';
+const questionTypeArr = ["【熟记】", "【必会】", "【理解】", "【领会】"];
+const right = "";
 
-let output = '';
-const $items = document.querySelectorAll('.data__items');
-Array.from($items).forEach(i => {
-  const $title = i.querySelector('.data__tit_cjd');
+let output = "";
+
+output += `${document.querySelector(".score-form__title").innerHTML}\n`;
+
+const $items = document.querySelectorAll(".data__items");
+Array.from($items).forEach((i) => {
+  const $title = i.querySelector(".data__tit_cjd");
   let title;
 
   // 处理没选项的情况
-  if (!($title && $title.childNodes)) return
+  if (!($title && $title.childNodes)) return;
 
-  if ($title.childNodes[0].tagName === 'LABEL') {
+  if ($title.childNodes[0].tagName === "LABEL") {
     title = $title.childNodes[1].data;
   } else {
     title = $title.childNodes[0].data;
   }
-  if (!questionTypeArr.some(q => title.includes(q)) && !title.includes('您的姓名') && !title.includes('联系电话')) {
+  if (
+    !questionTypeArr.some((q) => title.includes(q)) &&
+    !title.includes("您的姓名") &&
+    !title.includes("联系电话")
+  ) {
     const options = [];
 
-    const $right = i.querySelector('.judge_ques_right');
-    const isRight = Array.from($right.childNodes).some(r => r.innerText.includes('回答正确'));
+    const $right = i.querySelector(".judge_ques_right");
+    const isRight = Array.from($right.childNodes).some((r) =>
+      r.innerText.includes("回答正确")
+    );
 
     let answer;
 
-    const $ulradiochecks = i.querySelectorAll('.ulradiocheck > div');
-    Array.from($ulradiochecks).forEach(u => {
+    const $ulradiochecks = i.querySelectorAll(".ulradiocheck > div");
+    Array.from($ulradiochecks).forEach((u) => {
       options.push(u.innerText.slice(1));
       if (isRight && u.innerText.includes(right)) {
         answer = u.innerText[1];
@@ -38,22 +42,22 @@ Array.from($items).forEach(i => {
     });
 
     if (!isRight) {
-      const $rightAnswer = i.querySelector('.answer-ansys');
+      const $rightAnswer = i.querySelector(".answer-ansys");
       answer = $rightAnswer.childNodes[1].innerText[0];
     }
 
     let jiexi;
-    const $jiexi = i.querySelector('.jixi_content_box > .jiexi-content');
+    const $jiexi = i.querySelector(".jixi_content_box > .jiexi-content");
     if ($jiexi) {
       jiexi = $jiexi.innerText;
     }
 
     output += `${title}(${answer})\n`;
-    output += options.join('\n') + '\n';
+    output += options.join("\n") + "\n";
     if (jiexi) {
       output += `解析：${jiexi}\n`;
     }
-    output += '\n';
+    output += "\n";
   }
 });
 console.log(output);
